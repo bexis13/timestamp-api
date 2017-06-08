bb4ddb202f3ee3c231c520ce87cffc5a43186337
Abose U <bexis18@gmail.com>
2017-06-08 09:56:20 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(request, response){
+    response.send("hello world");
+});
+
+app.listen(8080, function(){
+    console.log("hello world app is listening on port 8080")
+})
+
