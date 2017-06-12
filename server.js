5050082e8b8e3c14e9b785648ec5c5eee8932952
Abose U <bexis18@gmail.com>
2017-06-12 08:10:03 +0000

    started using moment

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
+var moment = require("moment");
+moment().format();
 
 app.get("/", function(request, response){
-    response.send("hello world");
+    
 });
 
 app.listen(8080, function(){
-    console.log("hello world app is listening on port 8080")
+    console.log("hello world app is listening on port 8080");
 })
 
