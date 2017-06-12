3e8b9ea9d36bdb815e8a1076ab24081da5984696
Abose U <bexis18@gmail.com>
2017-06-12 09:30:57 +0000

    fix route for invalid dates, to return null for unix and natural time

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ var moment = require("moment");
 moment().format();
 
 app.get("/", function(request, response){
-    
+     console.log("worked");
+    // console.log(moment().format("MM-DD-YYYY"));
+});
+app.get("/:id", function(request, response){
+    var date = request.params.id;
+    if(moment(date).isValid()){
+        
+    }
+    else{
+        response.send(JSON.stringify({unix: null, natural: null}));
+    }
+    console.log(moment().format("MM-DD-YYYY"));
 });
 
 app.listen(8080, function(){
-    console.log("hello world app is listening on port 8080");
+    console.log("app is listening on port 8080");
 })
 
