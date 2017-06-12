76286b5f597b9973abbdc27ca4f855252bed9c76
Abose U <bexis18@gmail.com>
2017-06-12 10:35:00 +0000

    fix route for valid date, to return both unix time and natural time

--- server.js
+++ server.js
@@ -5,17 +5,32 @@ moment().format();
 
 app.get("/", function(request, response){
      console.log("worked");
-    // console.log(moment().format("MM-DD-YYYY"));
+  
 });
 app.get("/:id", function(request, response){
-    var date = request.params.id;
-    if(moment(date).isValid()){
-        
+    var naturalTimeEntered = false;
+    var date = request.params.id;//get the date user put in the url
+    date = Number(date);
+    if(date.toString() === "NaN"){ //if a natural date was entered
+        date = request.params.id; // leave it how the user entered it
+        var naturalTimeEntered = true;
+    }
+    if(moment(date).isValid()){//if its a valid date
+        if(naturalTimeEntered){ // if it was a valid and natural date entered
+            var unixTime = Number(moment(date).format("X")); //x to return the unix timestamp in milliseconds
+            var naturalTime = moment(date).format("LL"); //x to the date in natural date format
+            response.send({unix: unixTime, natural:naturalTime});
+        }
+        else{
+            var unixTime = Number(moment(date).format("x")); //x to return the unix timestamp in milliseconds
+            var naturalTime = moment(date).format("LL"); //x to the date in natural date format
+            response.send({unix: unixTime, natural:naturalTime});
+        }
     }
     else{
-        response.send(JSON.stringify({unix: null, natural: null}));
+        response.send({unix: null, natural: null});//return null when date is invalid
     }
-    console.log(moment().format("MM-DD-YYYY"));
+   
 });
 
 app.listen(8080, function(){
